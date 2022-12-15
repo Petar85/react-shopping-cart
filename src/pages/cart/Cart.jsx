3821efd333054f8706813abed2e76b64eb0504ff
Peter Pan <petarzivkovic85@gmail.com>
2022-12-14 22:19:09 -0800

    app functionality-wise seems done, user click on button to add item to cart, number of clicks determine item's quantity. Checkout button remains disabled for now.

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -2,9 +2,13 @@ import React, { useContext } from 'react';
 import { PRODUCTS } from "../../products";
 import { ShopContext } from "../../context/shop-context";
 import { CartItem } from "./cart-item";
+import { useNavigate } from "react-router-dom";
+
 
 export const Cart = () => {
-  const { cartItems } = useContext(ShopContext);
+  const { cartItems, getTotalCartAmount } = useContext(ShopContext);
+  const totalAmount = getTotalCartAmount();
+  const navigate = useNavigate();
   return (
     <div className='cart'>
       <div>
@@ -17,6 +21,15 @@ export const Cart = () => {
           }
         })}
       </div>
+      {totalAmount > 0 ? (
+      <div className="checkout">
+        <p> Subtotal: ${totalAmount}</p>
+        <button onClick={() => navigate("/")}> Continue Shopping </button>
+        <button> Checkout </button>
+      </div>
+      ) : (
+        <h1> Your Cart is Empty! </h1>
+      )}
     </div>
   )
 }
